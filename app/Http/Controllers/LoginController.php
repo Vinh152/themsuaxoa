f67faa6916d83bafb1c842e497e1543a814fa82e
doanweb113 <doanweb123@gmail.com>
2023-03-15 01:42:38 +0700

    up bai

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function singup(Request $request)
+    {
+        $password=Hash::make($request->password);
+        $user=new User();
+        $user->name=$request->txt;
+        $user->email=$request->email;
+        $user->password=$password;
+        $user->save();
+        return view("login");
+    }
+
+    public function singin(Request $request)
+    {
+        $credentials=$request->only('email', 'password');
+        if(Auth::attempt($credentials))
+        {
+            return redirect()->intended('dashboard');
+
+            // return redirect()->intended('dashboard')
+            //             ->with('message', 'Signed in!');
+        }
+        else{
+            return redirect('/');
+        }
+
+    }
+
+    public function dashboard(){
+        if(Auth::check())
+        {
+            return view("show");
+        }
+        else{
+           return redirect('/');
+        }
+    }
+}
---
+++ resources/views/show.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./CSS/show.css">
+</head>
+<body>
+    <div class="container">
+        <ul class="menu">
+            <li><a href="">Trang chủ</a></li>
+            <li><a href="">Thêm</a></li>
+        </ul>
+
+        <h1 class="tittle">Bảng chi tiết</h1>
+        <table>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Function</th>
+
+            <tr>
+                <td>1</td>
+                <td>Hello</td>
+                <td><a href="">Sửa</a> / <a href="">Xóa</a></td>
+            </tr>
+
+            
+        </table>
+    </div>
+</body>
+</html>
